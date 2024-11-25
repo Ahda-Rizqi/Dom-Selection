c2d81a513768b9cbc859ffcf6ec4025a8a7979c7
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2024-11-25 17:56:37 +0700

    "Melengkapi dom selection"

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Dom Selection
 
-// document.getElementById() -> Element
+// 1. document.getElementById() -> Element
 
 const judul = document.getElementById("judul");
 
@@ -8,7 +8,7 @@ judul.style.color = "red";
 judul.style.backgroundColor = "blue";
 judul.innerHTML = "Ahda Rizqi";
 
-// document.getElementsByTagName() -> HTMLColections
+// 2. document.getElementsByTagName() -> HTMLColections
 
 const p = document.getElementsByTagName("p");
 
@@ -20,14 +20,44 @@ for (let i = 0; i < p.length; i++) {
 const h1 = document.getElementsByTagName("h1")[0];
 h1.style.fontSize = "30px";
 
-// document.getElementsByClassName() -> HTMLCollection
+// 3. document.getElementsByClassName() -> HTMLCollection
 
 const p1 = document.getElementsByClassName("p1")[0];
 p1.innerHTML = "Ahda Rizqi";
 
+// 4. document.querySelector() -> element
 
-kesimpulan
+const p4 = document.querySelector("#b p");
+p4.style.color = "blue";
+p4.style.fontSize = "100px";
+
+const li2 = document.querySelector("section#b ul li:nth-child(2)");
+li2.style.color = "orange";
+
+// 5. document.querySelectorAll()
+
+const p2 = document.querySelectorAll("p");
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "blue";
+}
+
+// kesimpulan
 
 // document.getElementById = mengambil id dari html untuk dirubah(bersifat element karena id di html itu 1);
 // document.getElementsByTagName = mengambil tag(p,h1,Ul,li) untuk dirubah (bersifat HTMLCollection(Kumpulan node dengan tag yang sama) dan akan jadikan Array,jadi jika ingin manipulasi maka memerlukan index[]);
 // document.getElementsByClassName = mengambil class dari html untuk manipulasi (bersifat HTMLCollection jadi memerlukan index )
+// querySelector = bersifat element tunggal ,contoh  kalau kita manggil p maka akan memilih p pertama walau p nya ada banyak
+// querySelectorall = bersifat nodelist kenapa bersifat nodelist,karena bisa saja dalam 1 queryselectorall itu ada class,tag,id jadi lebih general
+
+// Urutan pengunaan yang efesien 
+
+// getElementById() — Paling Cepat & Spesifik. Pilih ID jika kamu tahu ID elemen yang ingin dipilih.
+// querySelector() — Lebih Fleksibel dan dapat memilih berdasarkan kombinasi tag, kelas, atau ID.
+// getElementsByClassName() / getElementsByTagName() — Baik untuk memilih banyak elemen berdasarkan tag atau class, tapi lebih terbatas.
+// querySelectorAll() — Paling Fleksibel, tetapi hasilnya NodeList yang tidak terupdate otomatis dan lebih berat.
+
+// Kenapa ?
+
+// Element adalah yang paling spesifik (biasanya satu elemen).
+// HTMLCollection adalah kumpulan elemen yang bersifat live.
+// NodeList adalah kumpulan node yang lebih fleksibel, tapi tidak terupdate otomatis (static).
